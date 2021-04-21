c802abb496568cfc77722e2dd8295c2c80f7d38a
Nicole C <nclotus11@gmail.com>
2021-04-21 18:03:06 -0400

    git commit -m "add Keys"

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ea1a434f918cf865a3d5dadb81938f80"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAOlGiKm7_0eUom7PEoFeafA5ceadCDjSg"
