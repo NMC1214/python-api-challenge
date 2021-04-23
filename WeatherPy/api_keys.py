df9dd5ed97f642a66c1f1f1fd65eefdf1febfba7
Nicole C <nclotus11@gmail.com>
2021-04-23 19:03:07 -0400

    Loop for query

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ea1a434f918cf865a3d5dadb81938f80"
+weather_api_key = "f0c97108cadcde6cc4b623796d6143a8"
 
 # Google API Key
 g_key = "AIzaSyAOlGiKm7_0eUom7PEoFeafA5ceadCDjSg"
