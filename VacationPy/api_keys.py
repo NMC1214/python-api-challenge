691fc8651319e7236e7196196cdd361d3841af3f
Nicole C <nclotus11@gmail.com>
2021-04-26 17:36:26 -0400

    remove api keys

--- VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "ea1a434f918cf865a3d5dadb81938f80"
-
-# Google API Key
-g_key = "AIzaSyCn5fKdmZmmQWsKI7UsI4uX51cUTyjMNco"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "f0c97108cadcde6cc4b623796d6143a8"
-
-# Google API Key
-g_key = "AIzaSyAOlGiKm7_0eUom7PEoFeafA5ceadCDjSg"
